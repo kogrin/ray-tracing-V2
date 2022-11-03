5317c5e04d024ae26a0872b9dd01dca0a23f7937
kogrin <kogron99@gmail.com>
2022-11-04 01:13:01 +0300

    added ray color computation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 mod ray;
 mod vec;
 
+use ray::Ray;
 use std::io::{stderr, Write};
 use vec::Color;
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = r.direction().normalized();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
     const IMAGE_WIDTH: u64 = 256;
